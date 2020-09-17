a22f08ed82ea4ac290022748632384b17242055e
frikit <osipovvictor1994@gmail.com>
2020-09-17 11:05:01 +0100

    bump gradle 1.3.70 -> 1.4.10 bump gradle 6.2.1 -> 6.6.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    kotlin("jvm") version "1.3.70"
+    kotlin("jvm") version "1.4.10"
 }
 
 group = "org.example"
@@ -12,19 +12,13 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-    testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-api", version = "5.5.2")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
+    testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-api", version = "5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
 }
 
-configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-}
-tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "1.8"
-    }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "1.8"
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
     }
 }
--- src/test/kotlin/com/github/interview/MainKtTest.kt
+++ src/test/kotlin/com/github/interview/MainKtTest.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test
 internal class MainKtTest {
 
     @Test
-    fun helloWorldReturnsPersonalizedMessage() {
+    fun testHelloWorldReturnsPersonalizedMessage() {
         println("1")
     }
 }
